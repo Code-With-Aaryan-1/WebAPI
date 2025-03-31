109e63a7585b201aa7df23e4e5d3601df4d36f95
Code-With-Aaryan-1 <aaryanohekar277@gmail.com>
2025-03-31 10:41:46 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+import asyncio
+import json
+from fastapi import FastAPI, WebSocket
+from typing import List
+
+app=FastAPI()
+
+movies=["Inception","Interstellar","Titanic","Avatar","The Dark Knight"]
+connected_clients: List[WebSocket]=[]
+
+async def send_movie_data():
+
+  while True:
+    if connected_clients:
+      movie_data={
+        "movie_name": random.choice(movies),
+        "user_id":random.randint(1,100),
+        "watch_time":random.randint(1,180)
+      }
+      json_data=json.dumps(movie_data)
+      for client in connected_clients:
+        await client.send_text(json_data)
+
+  await asyncio.sleep(2)
+
+@app.websocket("/stream/")
+async def stream_data(websocket:WebSocket):
+  await websocket.accept(
+  connected_clients.append(websocket)
+
+  try:
+    while True:
+      await asyncio.sleep(10)
+  except  Exception: 
+      pass 
+  finally:
+      conncted_clients.remove(websocket)
+
+@app.websocket("startup")
+async def start_stream():
+  asyncio.create_task(send_movie_data())
+    
