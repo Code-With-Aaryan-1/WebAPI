1c448a72cb299a2636ced2f489e906f11e17d40f
Code-With-Aaryan-1 <aaryanohekar277@gmail.com>
2025-04-06 11:53:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,47 @@
+
 import random
 import asyncio
 import json
 from fastapi import FastAPI, WebSocket
 from typing import List
+import uvicorn
 
-app=FastAPI()
+app = FastAPI()
 
-movies=["Inception","Interstellar","Titanic","Avatar","The Dark Knight"]
-connected_clients: List[WebSocket]=[]
+movies = ["Inception", "Interstellar", "Titanic", "Avatar", "The Dark Knight"]
+connected_clients: List[WebSocket] = []
 
 async def send_movie_data():
-
-  while True:
-    if connected_clients:
-      movie_data={
-        "movie_name": random.choice(movies),
-        "user_id":random.randint(1,100),
-        "watch_time":random.randint(1,180)
-      }
-      json_data=json.dumps(movie_data)
-      for client in connected_clients:
-        await client.send_text(json_data)
-
-  await asyncio.sleep(2)
+    while True:
+        if connected_clients:
+            movie_data = {
+                "movie_name": random.choice(movies),
+                "user_id": random.randint(1, 100),
+                "watch_time": random.randint(1, 180)
+            }
+            json_data = json.dumps(movie_data)
+            for client in connected_clients:
+                await client.send_text(json_data)
+        await asyncio.sleep(2)
 
 @app.websocket("/stream/")
-async def stream_data(websocket:WebSocket):
-  await websocket.accept()
-  connected_clients.append(websocket)
-  print("New Websocket Connection Established")
+async def stream_data(websocket: WebSocket):
+    await websocket.accept()
+    connected_clients.append(websocket)
+    print("New Websocket Connection Established")
 
-  try:
-    while True:
-      await ayncio.sleep(10)
-  except  Exception: 
-      pass
-  finally:
-      conncted_clients.remove(websocket)
+    try:
+        while True:
+            await asyncio.sleep(10)
+    except Exception:
+        pass
+    finally:
+        connected_clients.remove(websocket)
 
 @app.on_event("startup")
-async def start_stream():
-  asyncio.create_task(send_movie_data())
+async def startup():
+    asyncio.create_task(send_movie_data())
 
-if __name__=="__main__":
-  port=int(os.environ.get("PORT",10000))
-  uvicorn.run(app,host="0.0.0.0",port=port)
+if __name__ == "__main__":
+    port = 5000
+    uvicorn.run(app, host="0.0.0.0", port=port)
